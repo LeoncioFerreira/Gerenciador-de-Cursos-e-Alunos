7309d354e158e1ed72a5b6756a3b7b37c5f20916
Leôncio Ferreira <leoncio.ferreira@aluno.ufca.edu.br>
2025-11-25 16:07:26 -0300

    Adiciona classes Turma e Matricula

--- src/matricula.py
+++ src/matricula.py
@@ -12,4 +12,66 @@ class Matricula:
 
     É a ligação central entre Aluno e Turma.
     """
-    pass
+    # Método construtor
+    def __init__(self, aluno, turma):
+        self.aluno = aluno
+        self.turma = turma
+
+        # Inicializa atributos opcionais para uso futuro
+        self.__nota = None
+        self.__frequencia = None
+
+    @property
+    def aluno(self):
+        return self.__aluno
+
+    @aluno.setter
+    def aluno(self, valor):
+        if not isinstance(valor, Aluno):  # Validação para garantir que o objeto passado seja da classe Aluno
+            raise TypeError("Aluno deve ser um objeto da classe Aluno.")
+        self.__aluno = valor
+    
+    @property
+    def turma(self):
+        return self.__turma
+
+    @turma.setter
+    def turma(self, valor):
+        if not isinstance(valor, Turma): # Validação para garantir que o objeto passado seja da classe Turma
+            raise TypeError("Turma deve ser um objeto da classe Turma.")
+        self.__turma = valor
+
+    @property
+    def nota(self):
+        return self.__nota
+
+    @nota.setter
+    def nota(self, valor):
+        if valor is not None: # Validação para a nota ser entre 0 e 10
+            if not (0 <= valor <= 10): 
+                raise ValueError("A nota deve estar entre 0 e 10.")
+        self.__nota = valor
+    
+    @property
+    def frequencia(self):
+        return self.__frequencia
+
+    @frequencia.setter
+    def frequencia(self, valor):
+        if valor is not None: # Validação para a frequencia ser entre 0 e 100%
+            if not (0 <= valor <= 100):
+                raise ValueError("A frequência deve estar entre 0 e 100%.")
+        self.__frequencia = valor
+
+    def __eq__(self, other):
+        #Matrículas são iguais se:
+        #mesmo aluno
+        #mesma turma
+       
+        if not isinstance(other, Matricula):
+            return False
+        return self.aluno == other.aluno and self.turma == other.turma
+
+    def __str__(self):
+        return f"Matricula: {self.aluno.nome} → {self.turma.codigo_oferta}"
+    
--- src/turma.py
+++ src/turma.py
@@ -1,5 +1,5 @@
 from .oferta import Oferta
-
+from .curso import Curso # Importado para validação de tipo no setter
 class Turma(Oferta):
     """
     Representa uma turma aberta para matrícula.
@@ -12,4 +12,62 @@ class Turma(Oferta):
 
     Relaciona-se com Curso e Matricula.
     """
-    pass
+
+    def __init__(self, codigo_oferta, curso, semestre, dias_horarios,
+                 vagas, status="ABERTA", local=None):
+        super().__init__( # Inicializa a classe pai Oferta
+            codigo_oferta=codigo_oferta,
+            codigo_curso=curso.codigo,  
+            semestre=semestre,
+            dias_horarios=dias_horarios,
+            vagas=vagas,
+            status=status,
+            local=local,
+        )
+        self.__matriculas = []
+        self.curso = curso
+
+   
+    @property
+    def matriculas(self):
+        return self.__matriculas[:]
+
+    @matriculas.setter
+    def matriculas(self, lista):
+        raise AttributeError("A lista de matrículas não pode ser substituída diretamente.") # Validação para não ser possível atribuir a lista diretamente
+    
+    @property
+    def curso(self):
+        return self.__curso
+
+    @curso.setter
+    def curso(self, novo_curso):
+        if not isinstance(novo_curso, Curso):
+            raise TypeError("curso deve ser um objeto da classe Curso.") # Validação para garantir que é da classe Curso
+        self.__curso = novo_curso
+
+    def matricular(self, matricula):
+        from .matricula import Matricula # Importação local para evitar erro de importação circular 
+
+        if not isinstance(matricula, Matricula):
+            raise TypeError("O objeto não é uma Matrícula válida.") 
+
+        if self.status != "ABERTA": # Validação de status
+            raise ValueError("Turma fechada.")
+        
+        if len(self) >= self.vagas: # Validação de vagas
+            raise ValueError("Turma lotada.")
+        
+        if matricula in self.__matriculas: # Validação para não haver aluno repetido
+             raise ValueError("Aluno já matriculado.")
+
+        self.__matriculas.append(matricula) # Adiciona a matrícula após passar por todas as validações
+
+    def __len__(self):
+       return len(self.__matriculas)
+
+    def __str__(self): # Método __str__ que retorna uma string com código da turma, nome do curso, semestre e ocupação
+        return (
+            f"Turma {self.codigo_oferta} | Curso: {self.curso.nome} | "
+            f"Semestre: {self.semestre} | Ocupação: {len(self)}/{self.vagas}"
+        )
