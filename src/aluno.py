68878805ab291617ca56e446a1e270e613b1fe2c
Leôncio Ferreira <leoncio.ferreira@aluno.ufca.edu.br>
2025-11-25 16:01:27 -0300

    Adiciona classe Pessoa e subclasse Aluno

--- src/aluno.py
+++ src/aluno.py
@@ -11,4 +11,62 @@ class  Aluno(Pessoa):
 
     Será relacionado com Turma e Matricula.
     """
-    pass
+    #Metodo construtor
+    def __init__(self, nome, email, matricula):
+        super().__init__(nome, email)  # Herda nome e email da Pessoa (classe pai)
+        self.matricula = matricula
+        self.__historico = []
+
+    @property 
+    def matricula(self):
+        return self.__matricula  # Faz o método se comportar como atributo
+    
+    @matricula.setter
+    def matricula (self,matricula):
+        if not matricula.strip(): # Faz a validação para que matrícula não fique vazia
+            raise ValueError("A matricula não pode ser vazio")
+        self.__matricula = matricula
+    
+    @property
+    def historico(self):
+        return self.__historico[:] # Retorna uma copia segura do historico
+    
+    def adicionar_ao_historico(self, matricula):
+        # Adiciona uma matrícula já concluída ao histórico do aluno.
+        self.__historico.append(matricula)
+
+    def calcular_cr(self):
+        
+        # Coeficiente de Redimento = soma(nota * carga_horaria) / soma(carga_horaria)
+        
+        total_pesos = 0
+        total_acumulado = 0
+
+        for matricula in self.__historico:
+            # Ignora matrículas sem nota lançada
+            if matricula.nota is None:
+                continue
+
+            carga = matricula.turma.curso.carga_horaria # Acessa a carga horária do Curso através de : Matricula,Turma e Curso
+            total_pesos += carga
+            total_acumulado += matricula.nota * carga
+
+        if total_pesos == 0:
+            return 0
+        
+        return total_acumulado / total_pesos
+
+    # Metodo __lt__ (menor que) serve para comparar o cf do objeto com outro
+    def __lt__(self, other):
+        if not isinstance(other, Aluno):
+            return NotImplemented
+
+        cr_self = self.calcular_cr()
+        cr_other = other.calcular_cr()
+
+    # Primeiro compara pelo CR
+        if cr_self != cr_other:
+            return cr_self < cr_other  # menor CR = "menor" aluno na ordenação
+
+    # Desempate por nome
+        return self.nome < other.nome
--- src/pessoa.py
+++ src/pessoa.py
@@ -1,3 +1,4 @@
+import re
 class Pessoa:
     """
     Classe base que representa uma pessoa no sistema.
@@ -7,4 +8,34 @@ class Pessoa:
     - Armazenar informações comuns (nome, e-mail, etc.)
     - Servir como base para herança (extensibilidade)
     """
-    pass
+    # Método construtor
+    def __init__(self,nome,email):
+        self.nome = nome 
+        self.email = email
+
+    @property 
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome (self,nome):
+        # Validação para nome não ser vazio
+        if not nome.strip():
+            raise ValueError("O nome não pode ser vazio")
+        self.__nome = nome
+    
+    @property
+    def email(self):
+        return self.__email
+    @email.setter
+    def email(self, valor):
+        # Regex simples para validar e-mails no formato geral
+        padrao = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+
+        if not re.match(padrao, valor):
+            raise ValueError("E-mail inválido.")
+        self.__email = valor
+    
+    # Metodo __str__ que retorna uma string com nome e e-mail
+    def __str__(self):
+        return f"Pessoa: {self.__nome} <{self.__email}>"
