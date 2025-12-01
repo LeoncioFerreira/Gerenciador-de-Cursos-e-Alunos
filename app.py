6725f0f542514146d2215d948436c34e91572891
Leôncio Ferreira <leoncio.ferreira@aluno.ufca.edu.br>
2025-12-01 15:44:31 -0300

    Adiciona validações dos  setters de Curso e pré-requisitos

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from src.persistencia import carregar_cursos, salvar_cursos
+from src.curso import Curso
 
 app = Flask(__name__)
 
@@ -10,28 +11,56 @@ def index():
 @app.route("/cursos") # Rota para listar cursos cadastrados
 def cursos():
     lista = carregar_cursos()  # Pega os cursos do JSON com a função de atalho criada
+     # Cria um dicionário para buscar nome por código
+    mapa = {c["codigo"]: c["nome"] for c in lista}
+
+    # Constrói lista com nomes dos pré-requisitos
+    for c in lista:
+        nomes = []
+        for cod in c.get("pre_requisitos", []): # Pega os pre_requisitos daquela lista
+            nome_pr = mapa.get(cod, "Desconhecido") # Pega o nome do pre_requsitos se não existir vira Desconhecido
+            nomes.append(f"{cod} - {nome_pr}") # Adiciona "código - nome" na lista nomes
+        c["pre_requisitos_formatado"] = nomes # # Cria a chave pre_requisitos_formatado com a lista código - nome para mostrar no html
     return render_template("cursos.html", cursos=lista)
 
 @app.route("/cadastrar_curso", methods=["GET", "POST"])
 def cadastrar_curso():
-    if request.method == "POST": # Se o método for POST o formulário foi enviado
-       
-        codigo = request.form["codigo"]  # Obtém os dados do formulário enviados pelo usuário
-        nome = request.form["nome"]
-        ch = request.form["ch"]
+    if request.method == "POST": # Se o metodo for POST carrega o formulario
 
-        cursos = carregar_cursos()  # Carrega a lista atual de cursos do JSON
+        codigo = request.form["codigo"] # Pega o codigo do Html
+        nome = request.form["nome"] # Pega o nome do Html
 
-        novo = {           # Dicionário representando um novo curso
-            "codigo": codigo, 
-            "nome": nome,
-            "ch": int(ch) }
+        try:
+            carga_horaria = int(request.form["ch"]) # Pega carga horaria como int 
+        except ValueError:
+            return render_template("cad_curso.html", erro="Carga horária deve ser um número.") # Se não for int retorna o erro
 
-        cursos.append(novo) # Adiciona o novo curso à lista
-        salvar_cursos(cursos) # Atualiza o arquivo JSON com o novo curso
+        # Le lista de pre requisitos marcados
+        pre_requisitos = request.form.getlist("pre_requisitos") # Pega os pre requisitos do html
 
-        return redirect("/cursos") # Depois de salvar, redireciona para a lista de cursos
+        try:
+            curso_obj = Curso(codigo, nome, carga_horaria, pre_requisitos) # Transforma os dados do html em um objeto Curso (passa pelos setters)
 
-    return render_template("cad_curso.html") # Se for GET, exibe o formulário vazio
+        except Exception as e:
+            lista = carregar_cursos()
+            return render_template("cad_curso.html", erro=str(e), cursos=lista)# Se der erro mostra a mensagem gerada pelos setters em curso.py
+
+        novo = { # Cria dicionario do novo curso
+            "codigo": curso_obj.codigo,
+            "nome": curso_obj.nome,
+            "carga_horaria": curso_obj.carga_horaria,
+            "pre_requisitos": curso_obj.pre_requisitos
+        }
+
+        cursos = carregar_cursos()  
+        cursos.append(novo) # Adiciona o novo curso no formato do dicionario criado
+        salvar_cursos(cursos) # Salva a lista de cursos usando a função auxiliar de persistência
+
+        return redirect("/cursos")
+
+    # Manda lista de cursos para seleção
+    lista = carregar_cursos()
+    return render_template("cad_curso.html", cursos=lista)
+
 if __name__ == "__main__":
     app.run(debug=True)
