d14e11e451a6bcc2e7e4b2ecd8bcb54fdaf73616
Leôncio Ferreira <leoncio.ferreira@aluno.ufca.edu.br>
2025-12-02 17:31:36 -0300

    Adiciona testes para persistencia.py

---
+++ tests/test_persistencia.py
@@ -0,0 +1,25 @@
+import os
+import tempfile
+from src.persistencia import salvar, carregar
+
+def test_persistencia_salvar_e_carregar():
+    # Cria diretório e arquivo temporários
+    with tempfile.TemporaryDirectory() as tmpdir:
+        caminho = os.path.join(tmpdir, "teste.json")
+
+        dados_original = [
+            {"nome": "João", "matricula": "2025001"},
+            {"nome": "Maria", "matricula": "2025002"}
+        ]
+
+        # Salva no json
+        salvar(caminho, dados_original)
+
+        # Garante que o arquivo realmente existe
+        assert os.path.exists(caminho)
+
+        # Carrega do JSON
+        dados_carregados = carregar(caminho)
+
+        # Verifica se o conteúdo é igual
+        assert dados_carregados == dados_original
